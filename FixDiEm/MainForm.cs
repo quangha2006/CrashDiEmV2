e822729dbce584937bd170497ee6a626cf21364a
quang.haduy@gameloft.com <quang.haduy@gameloft.com>
2020-12-14 15:34:29 +0700

    Revert "Merge branch 'master' of https://github.com/quangha2006/CrashDiEmV2"
    This reverts commit d5a54a17c653dfdfd89e81d06296ce9b4cba2c9e, reversing
    changes made to 2eee2a1c327e0d26b56a176ba74b2b4e4a4511e8.

--- FixDiEm/MainForm.cs
+++ FixDiEm/MainForm.cs
@@ -403,7 +403,7 @@ namespace FixDiEm
 
                     dataToShow += "\r\n";
 
-                    string[] backTraces = analyzeData.GetBacktraceByID(data.IssueID);
+                    string[] backTraces = analyzeData.GetBacktraceByID(data.IssueID, appSettings);
                     if (backTraces != null)
                     {
 
@@ -587,20 +587,20 @@ namespace FixDiEm
                     dataToShow += ($"Architecture: {string.Join(",", Arch.ToArray())}\r\n");
                     dataToShow += ("*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***\r\nStacktrace:\r\n\r\n");
 
-                    int collectData_Count = issue.GetStactrace().Length;
-                    var stacktrace = issue.GetStactrace();
+                    var stacktrace = issue.GetStactrace(appSettings);
+                    int collectData_Count = stacktrace.Length;
                     for (int i = 0; i < collectData_Count; i++)
                     {
-                        string crashline = stacktrace[i] + "\r\n";
-                        if (checkBox_showAddress.Checked)
-                        {
-                            dataToShow += crashline;
-                        }
-                        else
-                        {
-                            dataToShow += RemoveAddressInCrashLine(crashline);
+                        dataToShow += stacktrace[i] + "\r\n";
+                        //if (checkBox_showAddress.Checked)
+                        //{
+                        //    dataToShow += crashline;
+                        //}
+                        //else
+                        //{
+                        //    dataToShow += RemoveAddressInCrashLine(crashline);
 
-                        }
+                        //}
                         if (numericUpDown_MaxLineOfStackToShow.Value > 0 && i > numericUpDown_MaxLineOfStackToShow.Value)
                         {
                             dataToShow += $"{collectData_Count - i} more lines....\r\n";
